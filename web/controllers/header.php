2c30a3f7f829702d575160e34905d2e98ce3bea5
Roberto Torella <robertot@square-enix.com>
2012-07-03 16:56:12 +0100

    Added background images and removed ratings

--- web/controllers/header.php
+++ web/controllers/header.php
@@ -1,10 +1,25 @@
 <?php
 
 $header=$app["controllers_factory"];
+$header->before(function () use ($app) {
+	$app["pippo"]=33;	
+});
 $header->get("/", function() use ($app) {
 	return $app["twig"]->render("header.html.twig",array(
-		"title"=>"Il cazzo",
+		"title"=>"lect.io",
+		"menu"=>array(
+			"login"=>array(
+				"title"=>"login",
+				"href"=>"#login",
+			),
+		),
+		"pippo"=>$app["pippo"],
 	));
+});
+$header->get("/cacca", function () use ($app) {
+	echo $app["pippo"];
+})->before(function() use ($app) {
+	$app["pippo"]=44;
 });
 
 return $header;
--- web/index.php
+++ web/index.php
@@ -17,7 +17,7 @@ $app->before(function () use ($app) {
 		'twig.class_path'   => __DIR__.'/../vendor/twig/lib',
 		// 'twig.options'      => array('cache' => __DIR__ .'/cache'),
 	));
-
+	$app["pippo"] = 12;
 	// Registering URLGenerator
 	$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 });
@@ -33,7 +33,7 @@ $app->mount("/content", include 'controllers/content.php');
 
 // Root
 $app->get('/', function () use($app) {
-	return $app["twig"]->render("home.html.twig");
+	return $app["twig"]->render("home.html.twig",array("pippo"=>$app["pippo"]));
 })->bind("home");
 
 // Run
